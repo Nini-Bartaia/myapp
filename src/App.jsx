380f0e17ef7b606d9d31c8721eb7660d0ec5a167
ninibartaia <ninibartaia5@gmail.com>
2023-06-12 22:09:05 +0400

    homework

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+    <header className="App-header">
+      <img src="assets/depositphotos_65828845-stock-photo-portfolio-written-on-notebook.jpg" alt='portfolio' />
+      <h1>My Portfolio</h1>
+      <p>Welcome to my portfolio page. Here is some information about me.</p>
+    </header>
+  </div>
+  );
+}
+
+export default App;
